f86210a8b62f42aa2af8579e1d6512ae485f77e7
Reza Ali Nirwansyah <rezaalinirwansyah@gmail.com>
2024-05-10 13:46:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('popupButton').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
+});
+
+document.getElementById('close').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+});
+
+// Selamat anda menemukan flag ketiga
+// 8401
